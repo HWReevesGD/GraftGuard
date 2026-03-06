f3e1e6e79ce9f4ac948d401aaff791f5896a706e
pft6734 <pft6734@rit.edu>
2026-03-06 11:16:23 -0500

    Created the classes

---
+++ GraftGuard/Enemy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraftGuard
+{
+    internal class Enemy : GameObject
+    {
+    }
+}

5a089fdb9110599a372feef66a59e0b4e7f73eb3
Boxypluto <122708916+Boxypluto@users.noreply.github.com>
2026-03-06 11:23:42 -0500

    Worked on GameObject

--- GraftGuard/GameObject.cs
+++ GraftGuard/GameObject.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,16 @@ namespace GraftGuard
 {
     internal class GameObject
     {
+        public Vector2 Position { get; set; }
+        public Vector2 Size { get; set; }
+        public Rectangle Hitbox => new Rectangle((int)Position.X, (int)Position.Y, (int)Size.X, (int)Size.Y);
+        public Texture2D Texture { get; set; }
+
+
+
+        public virtual void Draw(SpriteBatch batch)
+        {
+
+        }
     }
 }

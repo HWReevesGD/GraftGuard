d46845520529c7cb4ae08f80be3b32b0e384b2ae
Boxypluto <122708916+Boxypluto@users.noreply.github.com>
2026-03-06 11:18:42 -0500

    Organized Construction Classes

--- GraftGuard/Construction/TowerManager.cs
+++ GraftGuard/Construction/TowerManager.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GraftGuard
+namespace GraftGuard.Construction
 {
     internal class TowerManager
     {
--- GraftGuard/GameObject.cs
+++ GraftGuard/GameObject.cs
@@ -8,5 +8,6 @@ namespace GraftGuard
 {
     internal class GameObject
     {
+        
     }
 }

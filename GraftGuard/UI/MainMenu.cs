130a1f62a60b7405e2e194c2cc2f2fdbbb0f0aa8
you <144391848+you277@users.noreply.github.com>
2026-03-20 14:49:00 -0400

    create basic menu graphics

--- GraftGuard/UI/MainMenu.cs
+++ GraftGuard/UI/MainMenu.cs
@@ -5,24 +5,40 @@ using GraftGuard.Utility;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 
 namespace GraftGuard.UI;
 
 internal class MainMenu {
     private World backgroundWorld;
-    private InputManager inputManager; // input manager that will never update, because it's for the background (world needs it)
+    private InputManager idleInputManager; // input manager that will never update, because it's for the background (world needs it)
+    private InputManager inputManager;
     private TimeState timeState;
 
+    private static int titleTextTopPosition = 150;
+    private static int startTextBottomPosition = 150;
+    private static float cameraPanSpeed = 0.5f;
+    private static float textWaveSpeed = 3f;
+    private static float kerning = 5;
+
     private static Texture2D backgroundTexture;
+    private static string titleText = "GRAFTGUARD";
+    private static string startText = "< Press Enter to Begin >";
 
     public static void LoadContent(ContentManager content) {
         backgroundTexture = content.Load<Texture2D>("pixel");
     }
 
-    public MainMenu() {
-        backgroundWorld = new World();
-        inputManager = new InputManager();
-        timeState = TimeState.Night; // always night
+    public MainMenu(InputManager inputManager) {
+        this.backgroundWorld = new World();
+        this.idleInputManager = new InputManager();
+        this.inputManager = inputManager;
+        this.timeState = TimeState.Night; // always night
+        CreateTowers();
+    }
+
+    private void CreateTowers() {
+        // create random towers for the world
     }
 
     /// <summary>
@@ -30,10 +46,12 @@ internal class MainMenu {
     /// </summary>
     /// <param name="gameTime">gameTime from Game1 Update()</param>
     public void Update(GameTime gameTime) {
-        backgroundWorld.Update(gameTime, inputManager, timeState);
+        backgroundWorld.Update(gameTime, idleInputManager, timeState);
 
         // TODO: animate this
-        Vector2 position = new Vector2(5f, 5f);
+        double x = 200 + Math.Cos(gameTime.TotalGameTime.TotalSeconds / 3 * cameraPanSpeed) * 400 + 400;
+        double y = 200 + Math.Sin(gameTime.TotalGameTime.TotalSeconds * cameraPanSpeed) * 300 + 300;
+        Vector2 position = new Vector2((float)x, (float)y);
 
         backgroundWorld.Camera.Position = position - Interface.ScreenCenter;
     }
@@ -51,21 +69,39 @@ internal class MainMenu {
         // draw background world
 
         batch.End();
-
         // Draw by the Camera's Position
         batch.Begin(samplerState: SamplerState.PointWrap, transformMatrix: backgroundWorld.Camera.WorldToScreen);
-
         backgroundWorld.DrawCamera(batch, gameTime, timeState);
-
         batch.End();
 
         // draw menu items
 
-        Rectangle fullScreenRect = new Rectangle(0, 0, (int)Interface.Width, (int)Interface.Height);
-        Color bgColor = new Color(0, 0, 0, 128f);
+        batch.Begin();
 
+        Rectangle fullScreenRect = new Rectangle(0, 0, (int)Interface.Width, (int)Interface.Height);
+        Color bgColor = new Color(0, 0, 0, 0.75f);
         batch.Draw(backgroundTexture, fullScreenRect, bgColor);
 
-        batch.DrawString(Fonts.Arial, "MAIN MENU!!! PRESS ENTER TO START", Vector2.Zero, Color.White);
+        Vector2 textSize = Fonts.Arial.MeasureString(titleText) + new Vector2((titleText.Length - 1) * kerning, 0);
+        Vector2 leftPosition = new Vector2(Interface.Width / 2 - textSize.X / 2, titleTextTopPosition);
+
+        float leftSize = 0;
+
+        for (int i = 0; i < titleText.Length; i++) {
+            string character = titleText.Substring(i, 1);
+            float yOffset = (float)Math.Sin(gameTime.TotalGameTime.TotalSeconds * textWaveSpeed + i / 2) * 6;
+
+            Vector2 charSize = Fonts.Arial.MeasureString(character);
+            Vector2 position = leftPosition + new Vector2(leftSize + charSize.X + kerning, yOffset);
+            batch.DrawString(Fonts.Arial, character, position, Color.White);
+
+            leftSize += charSize.X + kerning;
+        }
+
+
+        Vector2 startTextSize = Fonts.Arial.MeasureString(startText);
+        Vector2 startTextPosition = new Vector2(Interface.Width / 2 - startTextSize.X / 2, Interface.Height - startTextSize.Y - startTextBottomPosition);
+
+        batch.DrawString(Fonts.Arial, startText, startTextPosition, Color.White);
     }
 }
---
+++ GraftGuard/UI/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿// pause menu
+
+using GraftGuard.Map;
+using GraftGuard.Utility;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace GraftGuard.UI;
+
+internal class PauseMenu {
+    private World world;
+
+    private static string text = "< Press Enter to Return to Play >\n< Press Esc to Exit >";
+
+    private static Texture2D backgroundTexture;
+
+    public static void LoadContent(ContentManager content) {
+        backgroundTexture = content.Load<Texture2D>("pixel");
+    }
+
+    public PauseMenu(World world) {
+        this.world = world;
+    }
+
+    public void Draw(SpriteBatch batch, GameTime gameTime, TimeState timeState) {
+
+        batch.End();
+        // Draw by the Camera's Position
+        batch.Begin(samplerState: SamplerState.PointWrap, transformMatrix: world.Camera.WorldToScreen);
+        world.DrawCamera(batch, gameTime, timeState);
+        batch.End();
+
+        // draw menu items
+
+        batch.Begin();
+
+        Rectangle fullScreenRect = new Rectangle(0, 0, (int)Interface.Width, (int)Interface.Height);
+        Color bgColor = new Color(0, 0, 0, 0.75f);
+        batch.Draw(backgroundTexture, fullScreenRect, bgColor);
+
+        Vector2 textSize = Fonts.Arial.MeasureString(text);
+        Vector2 position = new Vector2(Interface.Width / 2 - textSize.X / 2, Interface.Height / 2 - textSize.Y / 2);
+
+        batch.DrawString(Fonts.Arial, text, position, Color.White);
+    }
+}

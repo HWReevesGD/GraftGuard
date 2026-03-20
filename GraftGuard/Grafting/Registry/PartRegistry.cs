4b6f1331eae2ce41f3ffa836ef06707093bda1d7
HWReeves <askhudsonreeves@gmail.com>
2026-03-20 12:45:00 -0400

    Made some changes to allow for head and torso loading

--- GraftGuard/Grafting/Registry/PartRegistry.cs
+++ GraftGuard/Grafting/Registry/PartRegistry.cs
@@ -16,26 +16,7 @@ internal static class PartRegistry
     private static Random random = new Random();
     private static List<PartDefinition> _allParts = [];
     public static ReadOnlyCollection<PartDefinition> Parts => _allParts.AsReadOnly();
-    /// <summary>
-    /// Register a new <see cref="PartDefinition"/>
-    /// </summary>
-    /// <param name="name">Part name</param>
-    /// <param name="texture">Part texture</param>
-    /// <param name="type">Part Type, either Limb or Head</param>
-    /// <param name="baseDamage">Base damage of the part</param>
-    /// <param name="speedModifier">Part speed modifier</param>
-    /// <param name="armorModifier">Part armor modifier</param>
-    /// <param name="rangeModifier">Part range modifier</param>
-    /// <param name="criticalModifier">Part critical hit chance multiplier</param>
-    /// <param name="healthModifier">Part health modifier</param>
-    /* Preserving for now
-     * public static void Register(
-        string name, Texture2D texture, PartType type, float baseDamage, float speedModifier = 1.0f,
-        float armorModifier = 1.0f, float rangeModifier = 1.0f, float criticalModifier = 1.0f,
-        float healthModifier = 1.0f)
-    {
-        _allParts.Add(new PartDefinition(name, texture, type, baseDamage, speedModifier, armorModifier, rangeModifier, criticalModifier, healthModifier));
-    }*/
+
 
     /// <summary>
     /// Loads the JSON library exported from Grafter and populates the registry.
@@ -68,7 +49,7 @@ internal static class PartRegistry
                     data.TextureName,
                     data.PivotX,
                     data.PivotY,
-                    PartType.Limb,
+                    data.Type,
                     data.BaseDamage,
                     data.SpeedModifier,
                     data.ArmorModifier,

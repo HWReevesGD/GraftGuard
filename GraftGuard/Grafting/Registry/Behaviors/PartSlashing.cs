991010addd89bde6fded947078e18ab7417b2850
Boxypluto <122708916+Boxypluto@users.noreply.github.com>
2026-03-20 11:40:45 -0400

    Started Part Behaviors

---
+++ GraftGuard/Grafting/Registry/Behaviors/PartSlashing.cs
@@ -0,0 +1,29 @@
+﻿using GraftGuard.Grafting.Towers;
+using GraftGuard.Map;
+using GraftGuard.Map.Enemies;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraftGuard.Grafting.Registry.Behaviors;
+internal class PartSlashing : IPartBehavior
+{
+    public void Draw(Tower tower, GameTime time, SpriteBatch batch, World world, InputManager inputManager, TimeState state)
+    {
+        
+    }
+
+    public void OnHitEnemy(Tower tower, GameTime time, Enemy enemy, World world, InputManager inputManager, TimeState state)
+    {
+        
+    }
+
+    public void Update(Tower tower, GameTime time, World world, InputManager inputManager, TimeState state)
+    {
+        
+    }
+}

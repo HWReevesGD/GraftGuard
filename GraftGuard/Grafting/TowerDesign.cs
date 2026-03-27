1c5bb4ac3ac49571f46d4075fd757d51f73b8de6
Boxypluto <122708916+Boxypluto@users.noreply.github.com>
2026-03-27 12:34:39 -0400

    Finished Tower Grafting GUI

---
+++ GraftGuard/Grafting/TowerDesign.cs
@@ -0,0 +1,23 @@
+﻿using GraftGuard.Grafting.Registry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraftGuard.Grafting.Towers;
+
+namespace GraftGuard.Grafting;
+/// <summary>
+/// Class used for holding a single <see cref="Tower"/> design from the <see cref="TowerGraftingGUI"/>
+/// </summary>
+internal class TowerDesign
+{
+    public TowerDefinition Definition;
+    public List<PartDefinition> Parts;
+
+    public TowerDesign(TowerDefinition definition, List<PartDefinition> parts)
+    {
+        Definition = definition;
+        Parts = parts.ToList();
+    }
+}

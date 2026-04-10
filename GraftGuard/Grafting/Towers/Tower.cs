af26533cb535dc96f46180d6986ea510fa535b6d
Boxypluto <122708916+Boxypluto@users.noreply.github.com>
2026-04-09 23:14:48 -0400

    Turret now Tracks Enemies

--- GraftGuard/Grafting/Towers/Tower.cs
+++ GraftGuard/Grafting/Towers/Tower.cs
@@ -23,12 +23,14 @@ internal abstract class Tower : GameObject, IMouseDetectable
 {
     public static Texture2D TexturePlaceholderTower { get; private set; }
     public static Texture2D TexturePlaceholderGround { get; private set; }
+    public static Texture2D TTurret { get; private set; }
     public const int MaxParts = 4;
 
     public static void LoadContent(ContentManager content)
     {
         TexturePlaceholderTower = content.Load<Texture2D>("Placeholder/tower_placeholder");
         TexturePlaceholderGround = content.Load<Texture2D>("Placeholder/tower_placeholder_2");
+        TTurret = content.Load<Texture2D>("Tower/turret");
     }
 
     protected AttachedPart[] _attachedParts;

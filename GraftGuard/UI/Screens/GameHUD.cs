ac194183d823d15a672f633255f64d7006816efe
you <144391848+you277@users.noreply.github.com>
2026-04-14 18:52:20 -0400

    health hud draws the correct heart sprite now

--- GraftGuard/UI/Screens/GameHUD.cs
+++ GraftGuard/UI/Screens/GameHUD.cs
@@ -9,6 +9,7 @@ using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
 
 namespace GraftGuard.UI.Screens;
 
@@ -75,7 +76,111 @@ internal class GameHUD
         previousHearts = (int)Math.Ceiling((float)PlayerData.CurrentGame.Health / PlayerData.CurrentGame.MaxHealth * numHearts);
     }
 
-    public void Draw(DrawManager drawing, GameTime gameTime, bool active)
+    public void DrawHealth(DrawManager drawing, GameTime gameTime, bool active, float hudTopOffset)
+    {
+        int currentHearts = (int)Math.Ceiling((float)PlayerData.CurrentGame.Health / PlayerData.CurrentGame.MaxHealth * numHearts);
+
+        float pulseCycle = (float)gameTime.TotalGameTime.TotalSeconds % 1;
+        float pulseScale = Math.Max(((0.5f - pulseCycle) / 0.5f), 0) * 0.25f + 1;
+
+        for (int i = 0; i < Math.Max(previousHearts, currentHearts); i++)
+        {
+            Vector2 baseSize = heartSize * hudScale;
+            Vector2 scaledMargin = heartMargin * hudScale;
+            float scaledGap = heartGap * hudScale;
+
+            float wave = (float)Math.Sin(-gameTime.TotalGameTime.TotalSeconds * 2 + (float)i / 2) * 5;
+
+            // hearts that have been lost since  the last frame
+            if (i >= currentHearts)
+            {
+                Vector2 particleOrigin = scaledMargin + new Vector2(
+                    (baseSize.X + scaledGap) * i - baseSize.X / 2,
+                    wave - baseSize.Y / 2 + hudTopOffset
+                );
+
+                // particles for the hearts that the player lost
+                for (int j = 0; j < 10; j++)
+                {
+                    // big particle
+                    particles.Add(
+                        new Particle(Placeholders.TexturePixel)
+                            .SetLifetime(0.5f, 1f)
+                            .SetColor(Color.Red)
+                            .SetSize(Vector2.One * 100, Vector2.Zero)
+                            .SetSpeed(200f, 400f)
+                            .SetAngle(0, (float)Math.PI * 2)
+                            .SetPosition(particleOrigin)
+                        );
+
+                    // small particle
+                    particles.Add(
+                        new Particle(Placeholders.TexturePixel)
+                            .SetLifetime(0.3f, 2f)
+                            .SetColor(Color.Red)
+                            .SetSize(Vector2.One * 35, Vector2.Zero)
+                            .SetSpeed(400f, 800f)
+                            .SetAngle(0, (float)Math.PI * 2)
+                            .SetPosition(particleOrigin)
+                        );
+                }
+
+                continue;
+            }
+
+            // draw other hearts
+
+            Texture2D textureToDraw = heartSpriteSheet;
+            int spritesheetIndex = 0;
+
+
+            // render current heart in fractions
+            if (i == currentHearts - 1)
+            {
+                // figure out what spritesheet index is needed
+                // this took a bit of thinking so its thoroughly annotated
+
+                float percentSegmentSize = 1f / numHearts;
+                // overall health percentage
+                float healthPercent = (float)PlayerData.CurrentGame.Health / PlayerData.CurrentGame.MaxHealth;
+                // health percentage relevant to this heart
+                float percentMod = healthPercent % percentSegmentSize;
+                // as percentage of the percentage of one heart
+                float heartPercent = percentMod / percentSegmentSize;
+                // force round down (so that spritesheetIndex will not equal numHeartSegments)
+                spritesheetIndex = (int)Math.Round(heartPercent * numHeartSegments - 0.5f);
+            }
+            else
+            {
+                // full heart
+                spritesheetIndex = numHeartSegments - 1;
+            }
+
+            int xSegmentSize = heartSpriteSheet.Bounds.Width / numHeartSegments;
+
+            Rectangle sourceRect = new Rectangle(spritesheetIndex * xSegmentSize, (spritesheetIndex + 1) * xSegmentSize, xSegmentSize, heartSpriteSheet.Bounds.Height);
+            Vector2 finalSize = i == currentHearts - 1 ? baseSize * pulseScale : baseSize;
+            Vector2 position = scaledMargin + new Vector2(
+                (baseSize.X + scaledGap) * i - finalSize.X / 2,
+                wave - finalSize.Y / 2 + hudTopOffset
+            );
+
+            Rectangle rect = new Rectangle(
+                (int)position.X,
+                (int)position.Y,
+                (int)finalSize.X,
+                (int)finalSize.Y
+                );
+
+            drawing.Draw(textureToDraw, destination: rect, source: sourceRect, isUi: true, sortMode: SortMode.Top);
+            drawing.DrawString($"HEALTH: {PlayerData.CurrentGame.Health}", new Vector2(64, 64), isUi: true);
+        }
+
+        previousHealth = PlayerData.CurrentGame.Health;
+        previousHearts = currentHearts;
+    }
+
+    public void DrawTimer(DrawManager drawing, GameTime gameTime, bool active, float hudTopOffset)
     {
         int totalSeconds = (int)Math.Ceiling(PlayerData.CurrentGame.Timer);
         int displaySeconds = totalSeconds % 60;
@@ -83,139 +188,15 @@ internal class GameHUD
 
         if (previousSeconds != totalSeconds)
         {
+            // tick the timer
             previousSeconds = totalSeconds;
             timerTextYOffset = timerTextTickYOffset;
-        } else
+        }
+        else
         {
+            // ease back down
             timerTextYOffset = MathHelper.Lerp(timerTextYOffset, 0, Math.Min(gameTime.Delta() * timerTickRecoverSpeed, 1));
         }
-
-        // whole hud offset
-
-        if (hudPrevInactive != active)
-        {
-            hudPrevInactive = active;
-            hudActiveChangeTime = (float)gameTime.TotalGameTime.TotalSeconds;
-        }
-
-        float hudTopOffset;
-        float baseAlpha = Math.Min(((float)gameTime.TotalGameTime.TotalSeconds - hudActiveChangeTime) / hudMoveTime, 1);
-
-        float scaledInactiveTop = hudInactiveTopPosition * hudScale;
-
-        if (active)
-        {
-            float easeOutAlpha = 1 - (float)Math.Pow(1 - baseAlpha, 2);
-            hudTopOffset = MathHelper.Lerp(scaledInactiveTop, 0, easeOutAlpha);
-        } else
-        {
-            float easeInAlpha = (float)Math.Pow(baseAlpha, 2);
-            hudTopOffset = MathHelper.Lerp(0, scaledInactiveTop, easeInAlpha);
-        }
-
-        #region Health
-        if (PlayerData.CurrentGame.Time == TimeState.Night)
-        {
-
-            int currentHearts = (int)Math.Ceiling((float)PlayerData.CurrentGame.Health / PlayerData.CurrentGame.MaxHealth  * numHearts);
-
-            float pulseCycle = (float)gameTime.TotalGameTime.TotalSeconds % 1;
-            float pulseScale = Math.Max(((0.5f - pulseCycle) / 0.5f), 0) * 0.25f + 1;
-
-            for (int i = 0; i < Math.Max(previousHearts, currentHearts); i++)
-            {
-                Vector2 baseSize = heartSize * hudScale;
-                Vector2 scaledMargin = heartMargin * hudScale;
-                float scaledGap = heartGap * hudScale;
-
-                float wave = (float)Math.Sin(-gameTime.TotalGameTime.TotalSeconds * 2 + (float)i / 2) * 5;
-
-                // hearts that have been lost
-                if (i >= currentHearts)
-                {
-                    Vector2 particleOrigin = scaledMargin + new Vector2(
-                        (baseSize.X + scaledGap) * i - baseSize.X / 2,
-                        wave - baseSize.Y / 2 + hudTopOffset
-                    );
-
-                    // particles for the hearts that the player lost
-                    for (int j = 0; j < 10; j++)
-                    {
-                        // big particle
-                        particles.Add(
-                            new Particle(Placeholders.TexturePixel)
-                                .SetLifetime(0.5f, 1f)
-                                .SetColor(Color.Red)
-                                .SetSize(Vector2.One * 100, Vector2.Zero)
-                                .SetSpeed(200f, 400f)
-                                .SetAngle(0, (float)Math.PI * 2)
-                                .SetPosition(particleOrigin)
-                            );
-
-                        // small particle
-                        particles.Add(
-                            new Particle(Placeholders.TexturePixel)
-                                .SetLifetime(0.3f, 2f)
-                                .SetColor(Color.Red)
-                                .SetSize(Vector2.One * 35, Vector2.Zero)
-                                .SetSpeed(400f, 800f)
-                                .SetAngle(0, (float)Math.PI * 2)
-                                .SetPosition(particleOrigin)
-                            );
-                    }
-
-                    continue;
-                }
-
-                // draw other hearts
-
-                Texture2D textureToDraw = heartSpriteSheet;
-                int spritesheetIndex = 0;
-                
-
-                // render current heart in fractions
-                if (i == currentHearts - 1)
-                {
-                    float healthSectionSize = (float)PlayerData.CurrentGame.MaxHealth / numHeartSegments;
-                    float prevSegment = (float)Math.Floor(PlayerData.CurrentGame.Health / healthSectionSize);
-                    float currentSegment = (float)(PlayerData.CurrentGame.Health / healthSectionSize);
-                    float prog = (currentSegment - prevSegment) / healthSectionSize;
-                    //spritesheetIndex = PlayerData.CurrentGame.Health / PlayerData.CurrentGame.MaxHealth * numHeartSegments;
-                    spritesheetIndex = (int)Math.Round(prog);
-                }
-                else
-                {
-                    // full heart
-                    spritesheetIndex = numHeartSegments - 1;
-                }
-
-                int xSegmentSize = heartSpriteSheet.Bounds.Width / numHeartSegments;
-
-                Rectangle sourceRect = new Rectangle(spritesheetIndex * xSegmentSize, (spritesheetIndex + 1) * xSegmentSize, xSegmentSize, heartSpriteSheet.Bounds.Height);
-                Vector2 finalSize = i == currentHearts - 1 ? baseSize * pulseScale : baseSize;
-                Vector2 position = scaledMargin + new Vector2(
-                    (baseSize.X + scaledGap) * i - finalSize.X / 2,
-                    wave - finalSize.Y / 2 + hudTopOffset
-                );
-
-                Rectangle rect = new Rectangle(
-                    (int)position.X,
-                    (int)position.Y,
-                    (int)finalSize.X,
-                    (int)finalSize.Y
-                    );
-
-                drawing.Draw(textureToDraw, destination: rect, source: sourceRect, isUi: true, sortMode: SortMode.Top);
-                drawing.DrawString($"HEALTH: {PlayerData.CurrentGame.Health}", new Vector2(64 ,64), isUi: true);
-            }
-
-            previousHealth = PlayerData.CurrentGame.Health;
-            previousHearts = currentHearts;
-        }
-        #endregion
-
-
-        #region Timer HUD
 
         float sWidth = timerWidth * hudScale;
         float sHeight = timerHeight * hudScale;
@@ -266,7 +247,40 @@ internal class GameHUD
             (int)sHeight
             ), color: Color.White,
             isUi: true);
-        #endregion
+    }
+
+    public void Draw(DrawManager drawing, GameTime gameTime, bool active)
+    {
+
+        // whole hud offset
+
+        if (hudPrevInactive != active)
+        {
+            hudPrevInactive = active;
+            hudActiveChangeTime = (float)gameTime.TotalGameTime.TotalSeconds;
+        }
+
+        float hudTopOffset;
+        float baseAlpha = Math.Min(((float)gameTime.TotalGameTime.TotalSeconds - hudActiveChangeTime) / hudMoveTime, 1);
+
+        float scaledInactiveTop = hudInactiveTopPosition * hudScale;
+
+        if (active)
+        {
+            float easeOutAlpha = 1 - (float)Math.Pow(1 - baseAlpha, 2);
+            hudTopOffset = MathHelper.Lerp(scaledInactiveTop, 0, easeOutAlpha);
+        } else
+        {
+            float easeInAlpha = (float)Math.Pow(baseAlpha, 2);
+            hudTopOffset = MathHelper.Lerp(0, scaledInactiveTop, easeInAlpha);
+        }
+
+        if (PlayerData.CurrentGame.Time == TimeState.Night)
+        {
+            DrawHealth(drawing, gameTime, active, hudTopOffset);
+        }
+
+        DrawTimer(drawing, gameTime, active, hudTopOffset);
 
         particles.Update(gameTime);
         particles.Draw(drawing, gameTime, isUi: true);

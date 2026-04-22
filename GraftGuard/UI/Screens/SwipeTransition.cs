a45922b5681005f376121a07cb084f579fa66146
you <144391848+you277@users.noreply.github.com>
2026-04-22 17:35:08 -0400

    Remove unused SwipeTransition imports

--- GraftGuard/UI/Screens/SwipeTransition.cs
+++ GraftGuard/UI/Screens/SwipeTransition.cs
@@ -2,10 +2,6 @@
 using GraftGuard.Utility;
 using Microsoft.Xna.Framework;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GraftGuard.UI.Screens;
 
